74d4b7147327ea4971f2f8c8f18ad7f06e6b908d
Ale Roses <ale.vrs@outlook.com>
2025-09-13 15:17:06 -0500

    21.7 Cloudinary SDK - Delete image

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,3 @@
 // En caso de necesitar la implementación del FetchAPI
 import "whatwg-fetch"; // <-- yarn add whatwg-fetch
+import "setimmediate"; // npm i -D setimmediate
--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
+import { v2 as cloudinary } from "cloudinary";
 import { fileUpload } from "../../src/helpers/fileUpload";
+
+cloudinary.config({
+  cloud_name: "delkxyr6z",
+  api_key: "423579532613278",
+  api_secret: "k-ZUekWF7jbsR1Jm91rIe2P5cd8",
+  secure: true,
+});
 
 describe("Tests in fileUpload", () => {
   test("The file should upload correctly to Cloudinary.", async () => {
@@ -6,10 +14,37 @@ describe("Tests in fileUpload", () => {
       "https://www.online-image-editor.com/styles/2019/images/power_girl.png";
     const resp = await fetch(imageUrl);
     const blob = await resp.blob();
-    const file = new File([blob], "photo.jpg");
+    const file = new File([blob], "photo.png");
 
     const url = await fileUpload(file);
 
     expect(typeof url).toBe("string");
+
+    // console.log(url);
+    const segments = url.split("/");
+    const imageId = segments[segments.length - 1].replace(
+      ".png",
+      ""
+    );
+
+    // const cloudResp = await cloudinary.api.delete_resources(
+    //   ["journal/" + imageId],
+    //   {
+    //     resource_type: "image",
+    //   }
+    // );
+
+    const cloudResp = await cloudinary.api.delete_resources([
+      imageId,
+    ]);
+
+    console.log({ cloudResp });
+  });
+
+  test("It must return null.", async () => {
+    const file = new File([], "photo.png");
+    const url = await fileUpload(file);
+
+    expect(url).toBe(null);
   });
 });
