f4fc0b61b12e093302e9eb0ae57648e8c8a45ce5
Ale Roses <ale.vrs@outlook.com>
2025-09-18 08:22:12 -0500

    21.17 Testing and Development Environment variables

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,11 @@
 // En caso de necesitar la implementación del FetchAPI
-import "whatwg-fetch"; // <-- yarn add whatwg-fetch
+import "whatwg-fetch"; // yarn add whatwg-fetch
 import "setimmediate"; // npm i -D setimmediate
+
+require("dotenv").config({
+  path: ".env.test",
+});
+
+jest.mock("./src/helpers/getEnvironments", () => ({
+  getEnvironments: () => ({ ...process.env }),
+}));
