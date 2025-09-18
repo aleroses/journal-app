5ae105e6e81a3e23fe2e5606f8725e027ab563ee
Ale Roses <ale.vrs@outlook.com>
2025-09-18 11:06:33 -0500

    21.18 Configure Firebase environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,8 +4,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 import { getEnvironments } from "../helpers/getEnvironments";
 
-const env = getEnvironments();
-console.log(env);
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_DATABASEURL,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+} = getEnvironments();
+// console.log(env);
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,16 +31,29 @@ console.log(env);
 // Testing
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCYxZgelzrA7JRP0rrRtMVMlslgJY9I8jA",
+//   authDomain: "appcompras-518fb.firebaseapp.com",
+//   databaseURL:
+//     "https://appcompras-518fb-default-rtdb.firebaseio.com",
+//   projectId: "appcompras-518fb",
+//   storageBucket: "appcompras-518fb.firebasestorage.app",
+//   messagingSenderId: "365427579609",
+//   appId: "1:365427579609:web:49ef881499d7673fc67cc8",
+// };
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCYxZgelzrA7JRP0rrRtMVMlslgJY9I8jA",
-  authDomain: "appcompras-518fb.firebaseapp.com",
-  databaseURL:
-    "https://appcompras-518fb-default-rtdb.firebaseio.com",
-  projectId: "appcompras-518fb",
-  storageBucket: "appcompras-518fb.firebasestorage.app",
-  messagingSenderId: "365427579609",
-  appId: "1:365427579609:web:49ef881499d7673fc67cc8",
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  databaseURL: VITE_DATABASEURL,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
 };
+
+// console.log(firebaseConfig );
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
